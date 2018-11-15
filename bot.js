4ee424ccbc49792d645f75c6e8edbad40a5cdb1a
Jnaby <36682528+Jnaby@users.noreply.github.com>
2018-11-15 14:16:35 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,41 +2,43 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-const devs = ['483743143989084161' , '483743143989084161' , '483743143989084161' , '483743143989084161'];
-const adminprefix = "-";
+const adminprefix = "#";
+const developers = ['485554728349204500'];
+
+
+
+
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
+      if (!developers.includes(message.author.id)) return;
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+      message.channel.send(`Changing The Playing To : **${argresult}**`)
   } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+      message.channel.send(`Changing The Watching To : **${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+      message.channel.send(`Changing The Listening To : **${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+    client.user.setGame(argresult, "https://www.twitch.tv/inikolaiii");
+      message.channel.send(`Changing The Streaming To : **${argresult}**`)
   }
-  });
-
-  
-  client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
- 
-});
- 
-client.on('guildMemberAdd',async member => {
-  if(member.guild.id !== '501419525661786122') return;
-  setTimeout(function(){
-  member.guild.channels.find(r => r.id === '501420247392190465').send('You`re In  **Energy**  Welcome ..');
-},3000);
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}**`)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To : **${argresult}**`);
+}
 });
   
 
